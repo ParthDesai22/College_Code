bc6d2339b5dd5f9563669ce10019b849e91c52a5
ParthDesai22 <126049336+ParthDesai22@users.noreply.github.com>
2023-08-17 09:35:09 +0530

    FCFS

--- TE/SPOS/fcfs(1).java
+++ TE/SPOS/fcfs(1).java
@@ -1,45 +1,51 @@
-import java.util.*;
-
+import java.text.ParseException;
+import java.util.Scanner;
 class FCFS{
-    public static void main(String []args){
-    int sum;
-    int tat;
-    int [] Process={1,2,3,4,5};
-    int [] Arrival_Time={0,1,2,5,7};
-    int [] Brust_Time={4,7,2,6,4};
-    int [][]F;
-
-    for (int i =0;i<5;i++){
-        for (int j=0;j<5;j++){
-            F[i][j]=0;
-        }
-        
-    }    
-
-    for(int i =0;i<5;i++){
-        F[i][0]=(Process[i]);
-        F[i][1]=(Arrival_Time[i]);
-        F[i][2]=(Brust_Time[i]);
-    }
-
-    for (int i = 0;i<Brust_Time.length;i++){
-        sum += Brust_Time[i];
-         F[i][3] = sum; 
-      }
-
-    for(int i =0;i<5;i++){
-        tat=F[i][3]-F[i][1];
-        F[i][4]=(tat);
-    }  
-    
-
-    for (int i =0;i<5;i++){
-        System.out.print("\n");
-        for (int j=0;j<5;j++){
-            System.out.print(F[i][j] + "\t");
-        }
-
-    }
-    }
-}   }
+static void WaitingTime(int processes[], int n, int bt[] ,int wt[]){
+wt[0] = 0;
+for (int i=1;i<n;i++){
+wt[i] = bt[i-1] + wt[i-1];
+}
+}
+static void TurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]){
+for (int i=0;i<n;i++){
+tat[i] = bt[i] + wt[i];
+}
+}
+static void AverageTime(int processes[], int n, int bt[]){
+int wt[] = new int[n] , tat[] = new int[n];
+int total_wt=0,total_tat=0;
+WaitingTime(processes,n,bt,wt);
+TurnAroundTime(processes,n,bt,wt,tat);
+System.out.printf("Processes Burst time Waiting"+" time Turn around time\n");
+for (int i=0;i<n;i++){
+total_wt = total_wt + wt[i];
+total_tat = total_tat + tat[i];
+System.out.printf("  %d  ",(i+1));
+System.out.printf("          %d ",bt[i]);
+System.out.printf("          %d", wt[i]);
+System.out.printf("       %d\n",tat[i]);
+}
+float s = (float)total_wt / (float) n;
+int t= total_tat / n;
+System.out.printf("Average Waiting  Time Is = %f",s);
+System.out.printf("\n");
+System.out.printf("Average Turn Around Time Is = %d",t);
+}
+public static void main(String[] args) throws ParseException{
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter the number of processes: ");      
+int n = sc.nextInt();
+int processes[] =new int[20];
+int burst_time[] = new int[20];
+System.out.println("\nEnter the Process Name And Burst Time For Each Process.");       
+for (int i = 0; i < n; i++) {
+System.out.print("\nFor Process " + (i + 1));
+System.out.print("Process Name : ");
+processes[i] = sc.nextInt();
+System.out.print("Process Burst Time : ");
+burst_time[i] = sc.nextInt(); 
+} 
+AverageTime(processes,n,burst_time);
+}
 }
