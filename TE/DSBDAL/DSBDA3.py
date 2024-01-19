8ed09e38dff822fb8f72d1ab4711e5fa41970cab
ParthDesai22 <126049336+ParthDesai22@users.noreply.github.com>
2024-01-19 12:07:26 +0530

    Data Wrangling

--- TE/DSBDAL/DSBDA3.py
+++ TE/DSBDAL/DSBDA3.py
@@ -14,14 +14,26 @@ nan_count = df.isna().sum()
 #replacing null values with mean
 mean = df['Europe'].mean().round(2)
 print('Mean = ',mean)
-
-
 df['Europe'] = df['Europe'].replace(0,mean)
 for i in range (934,955):
     print(df['Europe'].iloc[i].round(2))
 
-#Last Observation Carried Forward
+#replacing null values with mean
+median = df['Japan'].median().round(2)
+print('Median = ',median)
+df['Japan'] = df['Japan'].replace(0,median)
 for i in range (934,955):
-    df['Japan'].replace(to_replace=0,method='ffill').values
-for i in range (934,955):    
     print(df['Japan'].iloc[i].round(2))
+
+#replacing null values with mean
+mode = df['Europe'].mode().round(2)
+print('Mode = ',mode)
+df['Rest of World'] = df['Rest of World'].replace(0,mean)
+for i in range (934,955):
+    print(df['Rest of World'].iloc[i].round(2))
+
+#Last Observation Carried Forward
+#for i in range (934,955):
+#    df['Japan'].replace(to_replace=0,method='ffill').values
+#for i in range (934,955):    
+#    print(df['Japan'].iloc[i].round(2))
