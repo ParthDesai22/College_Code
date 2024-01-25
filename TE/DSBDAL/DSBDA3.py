e152cc7866ca7d5a09c2a0ad54757f532fce9a97
ParthDesai22 <126049336+ParthDesai22@users.noreply.github.com>
2024-01-25 11:07:56 +0530

    Data Wrangling

--- TE/DSBDAL/DSBDA3.py
+++ TE/DSBDAL/DSBDA3.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-df = pd.read_csv('/home/admin1/Desktop/Data.csv')
+df = pd.read_csv('/home/admin1/Dataset/F21111022/Data.csv')
 #df = pd.read_excel("Data.ods", engine="odf")
 des_values = df.describe()
 #print(des_values,"/n")
@@ -12,28 +12,34 @@ nan_count = df.isna().sum()
 #print(df.info)
 
 #replacing null values with mean
-mean = df['Europe'].mean().round(2)
+mean = df['Europe'].mean()
 print('Mean = ',mean)
 df['Europe'] = df['Europe'].replace(0,mean)
 for i in range (934,955):
     print(df['Europe'].iloc[i].round(2))
 
 #replacing null values with mean
-median = df['Japan'].median().round(2)
+median = df['Japan'].median()
 print('Median = ',median)
 df['Japan'] = df['Japan'].replace(0,median)
 for i in range (934,955):
     print(df['Japan'].iloc[i].round(2))
 
-#replacing null values with mean
-mode = df['Europe'].mode().round(2)
+#replacing null values with mode
+mode = df['Europe'].mode()
 print('Mode = ',mode)
 df['Rest of World'] = df['Rest of World'].replace(0,mean)
 for i in range (934,955):
     print(df['Rest of World'].iloc[i].round(2))
 
 #Last Observation Carried Forward
-#for i in range (934,955):
-#    df['Japan'].replace(to_replace=0,method='ffill').values
-#for i in range (934,955):    
-#    print(df['Japan'].iloc[i].round(2))
+#df['Japan'] = df['Japan'].fillna(method='ffill')
+df['Japan']=df['Japan'].ffill()
+for i in range (934,955):    
+    print(df['Japan'].iloc[i])
+
+#Next Observation Crarried Backward
+#df['Japan']=df['Japan'].fillna(method='bfill')
+df['Japan']=df['Japan'].bfill()
+for i in range (934,955):    
+    print(df['Japan'].iloc[i])
